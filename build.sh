399d370df4a556d5a62ab77b77b34727ad42e4bf
David Millar <dave@davegoesthedistance.com>
2014-09-03 01:08:09 -0400

    Made build process unique and changed how built HTML file is created.

--- build.sh
+++ build.sh
@@ -1,14 +1,25 @@
-#! /bin/bash
+#!/bin/bash
 
 echo "Combining stylesheets...";
 
 cat css/style.css \
   > ./built/css/built.css;
 
+STYLE_SUM=`shasum ./built/css/built.css | awk '{print $1}'`;
+
 echo "Combining JavaScript...";
 
 cat js/jquery.js \
   js/scripts.js \
   > ./built/js/built.js;
+
+SCRIPT_SUM=`shasum ./built/js/built.js | awk '{print $1}'`;
+
+echo "Generating unique build HTML...";
+
+cat build_template.html \
+  | sed -e "s#{{SCRIPT_HASH}}#$SCRIPT_SUM#g" \
+  | sed -e "s#{{STYLE_HASH}}#$STYLE_SUM#g" \
+  > ./built/index.html;
 
 echo "Build complete!";
