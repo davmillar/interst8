01b43427f352465bf5ff8190b30202817157ac18
David Millar <dave@davegoesthedistance.com>
2014-09-02 23:19:14 -0400

    Game initialization setup.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var interst8 = {
+  /**
+   * Initializes the game
+   * @return {undefined}
+   */
+  initGame: function () {
+    console.log("initGame");
+
+  }
+};
+
+$(document)
+  .on('ready', interst8.initGame);
