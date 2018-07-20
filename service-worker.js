272db7e96e6669f7269a39db067cd49fe4ee6ceb
David Millar <dave@davegoesthedistance.com>
2018-07-19 19:47:14 -0500

    Improve offline capabilities with improved service worker.

--- service-worker.js
+++ service-worker.js
@@ -1,61 +1,92 @@
 // jshint esversion:6
 var me = self,
-    CACHE_NAME = 'interst8r',
+    CACHE_NAME = 'interst8t',
     urlsToCache = [
-      '/',
-      '/?utm_source=web_app_manifest',
-      '/index.html',
-      '/assets/Overpass_Regular-webfont.eot',
-      '/assets/Overpass_Regular-webfont.svg',
-      '/assets/Overpass_Regular-webfont.ttf',
-      '/assets/Overpass_Regular-webfont.woff',
-      '/css/overpass.css',
-      '/css/style.css',
-      '/js/game.js',
-      '/js/service-worker-handler.js',
-      '/data/levels.json',
-      '/manifest.json'
+        '/',
+        '/?utm_source=web_app_manifest',
+        '/index.html',
+        '/assets/Overpass_Regular-webfont.eot',
+        '/assets/Overpass_Regular-webfont.svg',
+        '/assets/Overpass_Regular-webfont.ttf',
+        '/assets/Overpass_Regular-webfont.woff',
+        '/css/overpass.css',
+        '/css/style.css',
+        '/js/game.js',
+        '/js/service-worker-handler.js',
+        '/data/levels.json',
+        '/manifest.json'
     ];
 
 me.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
+    event.waitUntil(precache());
 });
 
 me.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
+    event.respondWith(fromNetwork(event.request, 400).catch(function () {
+        return fromCache(event.request);
+    }));
 });
 
 me.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
         })
-      );
-    })
-  );
+    );
 });
 
-self.addEventListener('message', function(event) {
-  if (event.data.action === 'skipWaiting') {
-    self.skipWaiting();
-  }
+me.addEventListener('message', function(event) {
+    if (event.data.action === 'skipWaiting') {
+        me.skipWaiting();
+    }
 });
+
+function precache() {
+    return caches.open(CACHE_NAME)
+        .then(function(cache) {
+            return fetch('/data/levels.json').then(function(response) {
+                return response.json();
+            })
+            .then(function(levelManifest) {
+                var levelDataFiles = levelManifest.map(function (levelData) {
+                    return levelData.data;
+                });
+
+                urlsToCache = urlsToCache.concat(levelDataFiles);
+
+                return cache.addAll(urlsToCache);
+            });
+        });
+}
+
+function fromCache(request) {
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            return matching || Promise.reject('no-match');
+        });
+    });
+}
+
+function fromNetwork(request, timeout) {
+    return new Promise(function (fulfill, reject) {
+        var timeoutId = setTimeout(reject, timeout);
+        fetch(request).then(function (response) {
+            clearTimeout(timeoutId);
+            fulfill(response);
+        }, reject);
+    });
+}
+
+function update(request) {
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return fetch(request).then(function (response) {
+            return cache.put(request, response);
+        });
+    });
+}
